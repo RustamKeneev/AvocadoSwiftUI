eaf41e9630a25678bf464cfb7c1bf7d9a2a1ce36
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-03-16 23:09:02 +0600

    Initial Commit

---
+++ AvocadoSwiftUI/AvocadoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoSwiftUIApp.swift
+//  AvocadoSwiftUI
+//
+//  Created by Rustam Keneev on 16/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvocadoSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AvocadoSwiftUI
+//
+//  Created by Rustam Keneev on 16/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
