365a0aa49cd1212f8bd681e6506ef59b32406fd9
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-03-17 21:36:02 +0600

    start setting and display avocado view

--- AvocadoSwiftUI/View/AvocadoView.swift
+++ AvocadoSwiftUI/View/AvocadoView.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct AvocadoView: View {
+    //MARK: - PROPERTY
+    @State private var pulsateAnimation: Bool = false
+
     var body: some View {
-        Text("Avocados")
+        VStack {
+            
+            Spacer()
+            
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 8)
+                .scaleEffect(self.pulsateAnimation ? 1 : 0.9)
+                .opacity(self.pulsateAnimation ? 1 : 0.9)
+                .animation(Animation.easeOut(duration: 1.5).repeatForever(autoreverses: true))
+            
+            VStack {
+                Text("Avocados".uppercased())
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(Color.white)
+                    .padding()
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 4, x: 0, y: 4)
+                Text("""
+Creamy, green, and full of nutrients! 
+Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+""")
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(Color("ColorGreenLight"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, minHeight: 120)
+            }//: VSTACK
+            .padding()
+            
+            Spacer()
+            
+        }//: VSTACK
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        )
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .onAppear(perform: {
+            self.pulsateAnimation.toggle()
+        })
     }
 }
 
 #Preview {
     AvocadoView()
+        .previewDevice("IPhone 14 Pro")
+        .environment(\.colorScheme, .dark)
 }
